121b001cef438a3370aa862a08c4a8285f365ff5
Geoffrey Vedernikoff <yefim323@gmail.com>
2012-02-11 13:05:17 -0500

    made parser a lot better by having it recognize tags

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,5 @@
+var currentRow = 0;
+
 $(document).ready(function () {
 	var parser = new Parser($('#edit'), $('#preview'));
 	
@@ -10,18 +12,23 @@ $(document).ready(function () {
 		$('#edit').focus();
 	});
 	$('div.subtext').click(function() {
-		$('#edit').focus();
+		$('#edit').focus();	
 	});
 	
-	// prevents tabbing away, keycode = 9 = tab key	
+	// prevents tabbing away
 	$('#edit').keydown(function(event) {
 		var keyCode = event.keyCode || event.which; 
+		//tab key
 		if (keyCode == 9) {
 			event.preventDefault();
 		}
 	});
 	// updates the preview right after the edit box was updated
 	$('#edit').keyup(function(event) {
+		// clear edit box if only a single <br> exists in it
+		if ($('#edit').html() == '<br>') {
+			$('#edit').html('');
+		}
 		$('#preview').html($('#edit').html());
 		parser.parse();
 	});
--- public/js/parser.js
+++ public/js/parser.js
@@ -1,7 +1,13 @@
+var keyList = [];
+keyList[0] = 'job:';
+keyList[1] = '';
+
 var symbols = [];
-symbols[0] = '>';
-symbols[1] = '*';
-symbols[2] = '';
+symbols['job:'] = [];
+symbols['job:'][0] = 'company:';
+symbols['job:'][1] = 'title:';
+symbols['job:'][2] = 'date:';
+symbols['job:'][3] = 'description:';
 
 var Parser = function(inp, out) {
 	this.input = $(inp);
@@ -12,7 +18,32 @@ String.prototype.replaceAll = function(str1, str2, ignore) {
 	return this.replace(new RegExp(str1.replace(/([\/\,\!\\\^\$\{\}\[\]\(\)\.\*\+\?\|\<\>\-\&])/g,"\\$&"),(ignore?"gi":"g")),(typeof(str2)=="string")?str2.replace(/\$/g,"$$$$"):str2);
 };
 
+Parser.prototype.parseKey = function(key, block) {
+	var len = symbols[key].length;
+	for (var i = 0; i < len; i++) {
+		var sym = symbols[key][i];
+		var index = block.indexOf(sym);
+		if (index != -1) {
+			var str = block.substring(index + sym.length).replace('</div>','');
+			console.log(str);
+		}
+	}
+}
+Parser.prototype.parseBlock = function(block) {
+	//parse from 'job:' to the next <div>
+	// then from 'dates:' to the next <div> and so on
+	for (var i = 0; i < keyList.length; i++) {
+		if (block.indexOf(keyList[i]) != -1)
+			this.parseKey(keyList[i], block);
+	}
+}
 Parser.prototype.parse = function() {
-	var str = this.input.html().replaceAll(' ', ',');
-	this.output.html(str);
+	var input_html = this.input.html();
+	var blocks = [];
+	blocks = input_html.split('<div><br></div>');
+	var output_html = "";
+	for (var i = 0; i < blocks.length; i++) {
+		this.parseBlock(blocks[i]);
+	}
+	this.output.html(output_html);
 }
